9173ac06942b8376ef57fbb1c7067665b24b8c29
Christofble <christof.bless@uzh.ch>
2020-10-27 00:30:19 +0100

    make d3 network

--- campaign_network_d3/data_network_campaign.js
+++ campaign_network_d3/data_network_campaign.js
@@ -1,26 +1,57 @@
+/*global d3, $*/
+/*eslint no-undef: "error"*/
 
 $.getJSON( "../SampleCampaign.json",  data => {
   console.log("load data.")
   console.log(data);
-  network = () => {
-    nodes = data.Campaigns[0].CampaignCreator.map(d => Object.create(d));
-    svg = d3.select("#network-graph").append("svg")
-        .attr("viewBox", [0, 0, 500, 500]);
+  var network = () => {
+    const width = 500;
+    const height = 300;
+    const nodes = data.Campaigns[2].CampaignCreator.map(d => Object.create(d));
+    nodes.map((item, i) => {
+      item.x = 50+((width-100)/(nodes.length+1))*(i+1)
+      item.y = 50
+    });
+    const campaign_node = Object.create({Name:data.Campaigns[2].name,id:"campaign", x:250, y:250})
+    const links = nodes.map((item, i) => {
+      return {"id":i,"source":campaign_node, "target":item}
+    });
+    nodes.push(campaign_node);
 
-    node = svg.append("g")
+    console.log(links)
+    console.log(nodes)
+    const svg = d3.select("#network-graph").append("svg")
+        .attr("viewBox", [0, 0, width, height]);
+
+    const link = svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .selectAll("line")
+        .data(links)
+        .join("line")
+        .attr("x1", d => { return d.source.x; })
+        .attr("y1", d => { return d.source.y; })
+        .attr("x2", d => { return d.target.x; })
+        .attr("y2", d => { return d.target.y; })
+        .attr("stroke-width", 1)
+        .attr("stroke","#999")
+        .attr("stroke-opacity", 0.6);
+
+    const node = svg.append("g")
         .attr("stroke", "#fff")
         .attr("stroke-width", 1.5)
         .selectAll("circle")
         .data(nodes)
         .join("circle")
         .attr("r", 5)
+        .attr("cx",d => {return d.x})
+        .attr("cy",d => {return d.y})
         .attr("fill", "blue");
 
     node.append("title")
         .text(d => d.Name);
     return svg.node();
   }
- console.log(network())
- network()
+  network()
 
 });
