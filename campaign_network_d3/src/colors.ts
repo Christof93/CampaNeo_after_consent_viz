08ac838bf3692e29ffffb41308da5f46bdbba097
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-01-16 18:27:50 +0100

    Move color declarations.

---
+++ campaign_network_d3/src/colors.ts
@@ -0,0 +1,36 @@
+const legend_colors: any = {
+  'Fuel': {
+    'dark': '#23F0D5',
+    'light': '#007CBE'
+  },
+  'Speed': {
+    'dark': '#FBFB9C',
+    'light': '#F6AE2D'
+  },
+  'GPS': {
+    'dark': '#D597FF',
+    'light': '#F26419'
+  }
+}
+
+const colors: any = {
+  'light_mode': 'dark',
+  'background': {
+    'dark': '#032F5E',
+    'light': '#F5FBFF'
+  },
+  'campaign_banner': {
+    'dark': '#B3D2EC',
+    'light': ''
+  },
+  'center_in': {
+    'dark': 'black',
+    'light': 'black'
+  },
+  'center_out': {
+    'dark': '#CBF9F9',
+    'light': '#97D8C4'
+  }
+}
+
+export {colors, legend_colors}
--- campaign_network_d3/src/index.ts
+++ campaign_network_d3/src/index.ts
@@ -6,20 +6,8 @@ import { getSVG } from './import_svg'
 import * as canvas_particles from './particle_overlay'
 import * as interactive from './interactivity'
 import * as detail_overlay from './detailed_view'
+import {colors, legend_colors} from './colors'
 
-const legend_colors:any = {
-  'Fuel':{'dark':'#23F0D5','light':'#007CBE'},
-  'Speed':{'dark':'#FBFB9C','light':'#F6AE2D'},
-  'GPS':{'dark':'#D597FF','light':'#F26419'}
-}
-const colors:any = {
-  'light_mode':'dark',
-  'background':{'dark':'#032F5E','light':'#F5FBFF'},
-  'campaign_banner':{'dark':'#B3D2EC','light':''},
-  'center_in':{'dark':'black','light':'black'},
-  'center_out':{'dark':'#CBF9F9','light':'#97D8C4'}
-}
-export {colors, legend_colors}
 interactive.add_dev_control_panel()
 fetch('SampleCampaign.json')
   .then(res => res.json())
