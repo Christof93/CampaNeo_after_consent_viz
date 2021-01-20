0b937ab144f99919ebc57419ae1c4cd4178c7c6c
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2021-01-21 00:57:11 +0100

    Change detail overlay.

--- campaign_network_d3/src/detailed_view.ts
+++ campaign_network_d3/src/detailed_view.ts
@@ -4,14 +4,25 @@ import { COLORS, LEGEND_COLORS } from './colors'
 import { getSVG } from './import_svg'
 
 export const build_detailed_view = () => {
-  d3.select('#grid').append('ul')
-    .attr('class', 'detail-overlay hidden')
-    .style('background-color', colorMode === 'dark'? 'rgba(0, 0, 0, 0.6)' : 'rgba(200, 200, 200, 0.6)')
+  const overlay_container = d3.select('#grid').append('div')
+    .attr('class', 'detail-overlay-container hidden')
     .style('backdrop-filter', 'blur(10px)')
-    .style('padding', '1em')
+    .style('background-color', colorMode === 'dark'? 'rgba(0, 0, 0, 0.6)' : 'rgba(200, 200, 200, 0.6)')
+
+  overlay_container.append('span')
+    .attr('class', 'detail-overlay-title')
+
+  overlay_container.append('ul')
+    .attr('class', 'detail-overlay hidden')
 }
 
 export async function show(node_info:any): Promise<void> {
+  d3.select('.detail-overlay-container')
+    .attr('class', 'detail-overlay-container visible')
+
+  d3.select('.detail-overlay-title')
+    .html(node_info.Name)
+
   const timeline = d3.select('.detail-overlay')
     .attr('class','detail-overlay visible')
     .selectAll('.datapoint')
@@ -36,9 +47,16 @@ export async function show(node_info:any): Promise<void> {
     .style('color', (d: any) => LEGEND_COLORS[d.type.toLowerCase()][colorMode])
     .text((d:any) => `${d.type} (retrieved at ${d.retrievedAt})`)
 
+    d3.select('.color-mode')
+      .style('position', 'absolute')
+
 }
 
 export const hide = ():void => {
   d3.select('.detail-overlay').attr('class', 'detail-overlay hidden')
+  d3.select('.detail-overlay-container').attr('class', 'detail-overlay-container hidden')
   d3.selectAll('.datapoint').remove()
+
+  d3.select('.color-mode')
+    .style('position', 'inherit')
 }
