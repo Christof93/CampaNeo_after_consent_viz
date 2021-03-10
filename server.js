a2a9c3cba6350fc672956855343237cb6e41c14c
Christof93 <christofbless@gmail.com>
2021-03-10 22:45:00 +0100

    revert changes of gh pages commit

--- server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var graphdb = require("./graphdb");
-graphdb.getUsers();
-graphdb.getCampaigns();
-//# sourceMappingURL=server.js.map
